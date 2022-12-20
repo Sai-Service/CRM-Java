5af972f428f46d959a383f0abbc0b021e5ccb71a
Lenovo <Lenovo@DHURI-DINESH-151021>
2022-12-20 15:49:56 +0530

    SS Service History

--- src/main/java/com/sai/impl/CrmReportsImpl.java
+++ src/main/java/com/sai/impl/CrmReportsImpl.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.util.Map;
 import javax.sql.DataSource;
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
@@ -132,5 +133,26 @@ public ByteArrayInputStream getCrmInsRepDtls(Map<String, Object> parameters, Str
         return new ByteArrayInputStream(bytes);
 
     }         
+
+public ByteArrayInputStream getVehHistPdf(Map<String, Object> parameters, String fileNme) throws Exception {
+
+//           File file = ResourceUtils.getFile("classpath:\\serviceHistory\\SS_SERVICE_HISTORY.jrxml");
+//          File subFile = ResourceUtils.getFile("classpath:\\serviceHistory\\SS_SERVICE_HIS_DTLS.jrxml");
+        File file = ResourceUtils.getFile("classpath://serviceHistory//SS_SERVICE_HISTORY.jrxml");//linux
+        File subFile = ResourceUtils.getFile("classpath://serviceHistory//SS_SERVICE_HIS_DTLS.jrxml");//linux
+
+        System.out.println("VehicleHistory" + file.getAbsolutePath());
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JasperReport subJasperReport = JasperCompileManager.compileReport(subFile.getAbsolutePath());
+        parameters.put("subreportParameter", subJasperReport);
+        parameters.put("Subreport1", file.getParent() + "//SS_SERVICE_HIS_DTLS.jasper");
+        java.sql.Connection con = localDataSource.getConnection();
+        //JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(gpList);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, con);
+        byte[] bytes = JasperExportManager.exportReportToPdf(jasperPrint);
+        con.close();
+        return new ByteArrayInputStream(bytes);
+
+    }
         
 }
